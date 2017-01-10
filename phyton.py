2e261d0c2675b218c9b535685332812aee8abe15
Harue84 <ju_watanabe84@hotmail.com>
2017-01-10 04:19:48 -0200

    Add files via upload

---
+++ phyton.py
@@ -0,0 +1,60 @@
+Python 2.7.12 (default, Nov 19 2016, 06:48:10) 
+[GCC 5.4.0 20160609] on linux2
+Type "copyright", "credits" or "license()" for more information.
+>>> print "Hello World!"
+Hello World!
+>>> pront "Hello World!"
+SyntaxError: invalid syntax
+>>> print 5 + 3
+8
+>>> 
+>>> print 5 x 3
+SyntaxError: invalid syntax
+>>> print 5 * 3
+15
+>>> print 2345 * 6789
+15920205
+>>> print 1234567898765432123456789 * 98765432123456789876543211219326320073159600609652202408166072245112635269
+121932632007315960006096523529422793604558515056218756870496806230355535998261707324848101638891241
+>>> print cat + dog
+
+Traceback (most recent call last):
+  File "<pyshell#8>", line 1, in <module>
+    print cat + dog
+NameError: name 'cat' is not defined
+>>> print "cat" + "dog"
+catdog
+>>> print "hello" * 20
+hellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohello
+>>> 
+======================= RESTART: /home/harue/trial.py =======================
+I love pizza!
+pizzapizzapizzapizzapizzapizzapizzapizzapizzapizzapizzapizzapizzapizzapizzapizzapizzapizzapizzapizza
+yumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyum
+I'm full.
+>>> 
+======================= RESTART: /home/harue/trial.py =======================
+I love pizza!
+pizzapizzapizzapizzapizzapizzapizzapizzapizzapizzapizzapizzapizzapizzapizzapizzapizzapizzapizzapizza
+yumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyum
+I'm full.
+Hello, and welcome to Phyton!
+I hope you will enjoy learning program.
+Bye for now!
+>>> 
+======================= RESTART: /home/harue/trial.py =======================
+I love pizza!
+pizzapizzapizzapizzapizzapizzapizzapizzapizzapizzapizzapizzapizzapizzapizzapizzapizzapizzapizzapizza
+yumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyum
+I'm full.
+Hello, and welcome to Phyton!
+I hope you will enjoy learning program.
+Bye for now!
+Hello, and welcome to Phyton!
+I hope you will enjoy learning to program.
+
+Traceback (most recent call last):
+  File "/home/harue/trial.py", line 11, in <module>
+    print "Bye for now!" + 5
+TypeError: cannot concatenate 'str' and 'int' objects
+>>> 
