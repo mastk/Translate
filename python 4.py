6f9fa6380b31b1665cbdff5baa93b1e57c20e64d
Harue84 <ju_watanabe84@hotmail.com>
2017-01-12 01:22:28 -0200

    Add files via upload

---
+++ python 4.py
@@ -0,0 +1,28 @@
+Python 2.7.12 (default, Nov 19 2016, 06:48:10) 
+[GCC 5.4.0 20160609] on linux2
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+============= RESTART: /home/harue/Four program/Four program.py =============
+Mr. Morton
+53 + 28
+81
+8
+8
+53
+catdog
+Sing a song of sixpence, a pocket full of rye,
+Four and twenty black birds baked in a pie
+When the pie was opened the birds began to sing,
+Wasn't that a dainty dish to set before the king?
+Teacher
+8
+Wicked
+How to kill a mockingbird
+This time, this place misused,mistakes
+Too long, too late, who was I to make you wait.
+Just one chance, just one breath
+Just in case there's just one left
+'Cause you know, you know, you know
+10080
+10920
+>>> 
