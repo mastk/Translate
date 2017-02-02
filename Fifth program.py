015b9bb7915dd2e8e26663431e20dbcda64b4558
Harue84 <ju_watanabe84@hotmail.com>
2017-02-02 18:21:50 -0200

    Dúvidas
    Não consegui entender essa função de float.

---
+++ Fifth program.py
@@ -0,0 +1,34 @@
+a = 24
+b = float(a)
+print a
+print b
+c = 38.0
+d = int(c)
+print d
+e = 54.99
+f = int(e)
+print e
+print f
+
+a = '76.3'
+b = float(a)
+print a
+print b
+
+a = '44.2'
+b = 44.2
+print type(a)
+print type(b)
+
+print float("fred")
+print float ("fred")
+
+cel = 5.0/9*(fahr - 32)
+cel = float(5)/9*(fahr - 32)
+cel = 5/float(9) * (fahr - 32)
+
+a = 78.9
+b = int(a)
+print a
+cel = float(5/9 * (fahr - 32))
+cel = 5/9 * float(fahr - 32)
