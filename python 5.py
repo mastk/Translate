cbc85fa24100144e7c223c26663a3e92be6738fa
Harue84 <ju_watanabe84@hotmail.com>
2017-02-02 18:26:14 -0200

    Add files via upload

---
+++ python 5.py
@@ -0,0 +1,20 @@
+Python 2.7.12 (default, Nov 19 2016, 06:48:10) 
+[GCC 5.4.0 20160609] on linux2
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+=================== RESTART: /home/harue/Fifth program.py ===================
+24
+24.0
+38
+54.99
+54
+76.3
+76.3
+<type 'str'>
+<type 'float'>
+
+Traceback (most recent call last):
+  File "/home/harue/Fifth program.py", line 23, in <module>
+    print float("fred")
+ValueError: could not convert string to float: fred
+>>> 
