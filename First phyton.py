2f7eef32b40048a6ebcfd1cb23eb546067f50bb8
Harue84 <ju_watanabe84@hotmail.com>
2017-03-09 07:36:31 -0300

    Add files via upload

---
+++ First phyton.py
@@ -0,0 +1,29 @@
+Python 2.7.12 (default, Nov 19 2016, 06:48:10) 
+[GCC 5.4.0 20160609] on linux2
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+============ RESTART: /home/harue/First program/First program.py ============
+I love pizza!
+pizzapizzapizzapizzapizzapizzapizzapizzapizzapizzapizzapizzapizzapizzapizzapizzapizzapizzapizzapizza
+yumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyumyum
+I'm full.
+Hello, and welcome to Phyton!
+I hope you will enjoy learning program.
+Bye for now!
+Try it out
+num modo interativo, use o Python para calcular os minutos na semana
+10080
+My name is Harue Matsumoto 
+I was born on September 18, 1984 
+My favorite color is purple.
+AHOY! I'm the Dread Pirate Roberts, and I have a secret!
+It's a number from 1 to 99. I'll give you 6 tries. 
+ What's yer guess? 2
+Too low, ye scurvy dog!
+No more guesses! Better luck next time, matey!
+The secret number was 40
+ What's yer guess? 56
+Too high, landlubber!
+No more guesses! Better luck next time, matey!
+The secret number was 40
+ What's yer guess? 
