e682fc776d8086f8410601f0165c9b189ca721bb
Mastk <mastk@users.noreply.github.com>
2017-03-11 21:51:08 -0300

    Update First chapter.py
    O programa do pirata funciona agora, os problemas eram os seguintes:
    O incremento nas tentativas
    O ponto que "else" e suas instruções ficaram.

--- First chapter.py
+++ First chapter.py
@@ -20,24 +20,22 @@ secret = random.randint (12, 44)
 guess = 0
 tries = 0
 
+
 print "AHOY! I'm the Dread Pirate Roberts, and I have a secret!"
 print "It's a number from 1 to 99. I'll give you 6 tries. "
 
 while guess != secret and tries < 6:
     guess = input (" What's yer guess? ")
+    tries = tries + 1
     if guess < secret:
         print "Too low, ye scurvy dog!"
     elif guess > secret:
-            print "Too high, landlubber!"
-            tries = tries + 1
-    if guess == secret:
+        print "Too high, landlubber!"
+    elif guess == secret:
         print "Avast! Ye got it! Found my secret, ye did!"
-    else:
-        print "No more guesses! Better luck next time, matey!"
-        print "The secret number was" , secret
-
-
-
+else:
+     print "No more guesses! Better luck next time, matey!"
+     print "The secret number was" , secret
 
 
 
