e18c148a4bc3c24b6a949ed877e9c758043d6609
Harue84 <ju_watanabe84@hotmail.com>
2017-06-04 22:42:29 -0300

    Add files via upload

---
+++ Fifth chapter.py
@@ -0,0 +1,69 @@
+print "Enter your name: "
+somebody = raw_input ()
+print "Hi", somebody, "how are you today?"
+
+print "Enter your last name: "
+lastName = raw_input ()
+
+print "Enter your age: ",
+age = raw_input ()
+
+print "My",
+print "name",
+print "is",
+print "Dave."
+
+print "This program converts Fahrenheit to Celsius"
+print "Type in a temperature in Fahrenheit: ",
+fahrenheit = float (raw_input())
+celsius = (fahrenheit - 32) * 5.0 / 9
+print "That is",
+print celsius,
+print "degree Celsius"
+
+response = raw_input ("How many students are in your class: ")
+numberOfStudents = int(response)
+
+
+import urllib
+file = urllib.urlopen ('http://helloworldbook.com/data/message.txt')
+message = file.read()
+print message
+
+print "Test your knowledge"
+
+somePlanet = raw_input ("Enter your planet: ")
+
+
+response = raw_input ("How many books are in your house: ")
+numberOfBooks = int(response)
+
+print "Try it out"
+
+long_string = ''' Num modo interativo, faca duas variaveis, uma com o seu
+primeiro nome e outro para o seu sobrenome. Depois, usando apenas um print para
+que apareca seu nome e sobrenome juntos.'''
+
+print long_string
+
+print "Enter your name: "
+someName = raw_input ()
+print "Enter your lastname: "
+someLastName = raw_input ()
+
+print someName ,
+print someLastName
+
+long_string = '''Escreva um programa que pergunte seu primeiro nome. Depois pergunte
+seu sobrenome e em seguida escreva uma mensagem com o seu nome e sobrenome.'''
+print long_string
+
+print "Enter your name: "
+someName = raw_input ()
+print "Enter your lastname: "
+someLastName = raw_input ()
+
+print someName ,
+print someLastName ,
+print "how is your day so far?"
+
---
+++ Fifth python.py
@@ -0,0 +1,43 @@
+Python 2.7.12 (default, Nov 19 2016, 06:48:10) 
+[GCC 5.4.0 20160609] on linux2
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+============ RESTART: /home/harue/Fifth program/Fifth chapter.py ============
+Enter your name: 
+Megumi
+Hi Megumi how are you today?
+Enter your last name: 
+Tadokoro
+Enter your age:  31
+My name is Dave.
+This program converts Fahrenheit to Celsius
+Type in a temperature in Fahrenheit:  18
+That is -7.77777777778 degree Celsius
+How many students are in your class: 32
+Congratulations! You have just made your computer reach across the internet to get a secret message!
+I hope you are enjoying "Hello World! Computer Programming for Kids and Other Beginners".
+Have fun!
+
+Warren and Carter
+
+
+Test your knowledge
+Enter your planet: Saturn
+How many books are in your house: 26
+Try it out
+ Num modo interativo, faca duas variaveis, uma com o seu
+primeiro nome e outro para o seu sobrenome. Depois, usando apenas um print para
+que apareca seu nome e sobrenome juntos.
+Enter your name: 
+Megumi
+Enter your lastname: 
+Tadokoro
+Megumi Tadokoro
+Escreva um programa que pergunte seu primeiro nome. Depois pergunte
+seu sobrenome e em seguida escreva uma mensagem com o seu nome e sobrenome.
+Enter your name: 
+Megumi
+Enter your lastname: 
+Tadokoro
+Megumi Tadokoro how is your day so far?
+>>> 
