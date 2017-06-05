8105fb77a32ce6a5a2a2510379ccb429b7c7b2af
Harue84 <ju_watanabe84@hotmail.com>
2017-06-04 22:21:37 -0300

    Add files via upload

---
+++ Third chapter.py
@@ -0,0 +1,79 @@
+print 8 - 5
+print 6 / 2
+print 3 / 2
+print 3.0 / 2
+print 2 + 3 * 4
+print (2 + 3) * 4
+print 3 * 3 * 3 * 3 * 3
+print 3 ** 5
+print 3 ** 5.5
+print 7 % 2
+print 7 / 2
+print 7.0 / 2
+number = 7
+number += 1
+print number
+number = 7
+number -= 1
+print number
+print 9938712345656.34 * 4823459023067.456
+a = 2.5e6
+b = 1.2e7
+print a + b
+c = 2.6e75
+d = 1.2e74
+print c + d
+print 8 / 3
+print 8.0 / 3
+print 6 ** 4
+print 17e6
+print 000004.56
+
+print "Try it out"
+
+long_string =  '''Três pessoas querem comer num restaurante e dividir a conta em três. O total é 35,27 e eles querem deixar 15% de gorjeta. Quanto cada
+pessoa pagará?'''
+
+print long_string
+
+First = 35.27
+Second = 3
+Fourth = 0.15
+Third = First / Second
+print Third
+Fifth = Third * Fourth
+print Fifth
+print Fifth + Third
+
+long_string = '''Calcule a área e perímetro de uma sala
+retangular de 12,5 metros por 16,7 metros.'''
+
+print long_string
+
+
+a = 12.5 * 16.7
+print a
+
+p = 12.5 + 12.5 + 16.7 + 16.7
+print p
+
+long_string = '''Escreva um programa para converter temperatura de Fahrenheit
+para Celsius. A fórmula que é: c = 5/9*(F-32).'''
+
+print long_string
+
+a = 41
+b = 32
+c = 1.8
+print a - b / 1.8
+a = 5
+b = 1.8
+c = 32
+print a * b + c
+
+long_string = '''Você consegue descobrir quanto tempo levará para chegar em algum
+lugar de carro? A fórmula (em palavras) é "o tempo de viagem é igual a distância
+dividida pela velocidade". Faça um programa para calcular o tempo para dirigir
+200km por 80km por hora e coloque a resposta.'''
+
+print long_string
